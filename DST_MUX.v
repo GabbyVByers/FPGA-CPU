b74dec676702dc314b6ea6ca03c85677e66a6d7e
Gabby <gabbyvbyers@gmail.com>
2025-10-05 14:54:49 -0700

    Add files via upload

--- DST_MUX.v
+++ DST_MUX.v
@@ -7,9 +7,9 @@ module DST_MUX (
 	output wire [15:0] DST
 );
 
-assign DST = (DST_SELECTION == 2'd0)? DTS0 :
-             (DST_SELECTION == 2'd1)? DTS1 :
-             (DST_SELECTION == 2'd2)? DTS2 :
-             (DST_SELECTION == 2'd3)? DTS3 : DST0;
+assign DST = (DST_SELECTION == 2'd0)? DST0 :
+             (DST_SELECTION == 2'd1)? DST1 :
+             (DST_SELECTION == 2'd2)? DST2 :
+             (DST_SELECTION == 2'd3)? DST3 : DST0;
 
 endmodule
