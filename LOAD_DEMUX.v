fed92c451199dd8c2b8931f6179ad95d104051dd
Gabby <gabbyvbyers@gmail.com>
2025-10-05 15:50:02 -0700

    Add files via upload

--- LOAD_DEMUX.v
+++ LOAD_DEMUX.v
@@ -1,15 +1,15 @@
 module LOAD_DEMUX (
-	input  wire [1:0] Select,
-	input  wire       Load_DST,
-	output wire       Load_R0,
-	output wire       Load_R1,
-	output wire       Load_R2,
-	output wire       Load_R3
+	input  wire [1:0] DST_SELECTION,
+	input  wire       LOAD_DST,
+	output wire       LOAD_R0,
+	output wire       LOAD_R1,
+	output wire       LOAD_R2,
+	output wire       LOAD_R3
 );
 
-assign Load_R0 = (Select == 2'd0)? Load_DST : 0;
-assign Load_R1 = (Select == 2'd1)? Load_DST : 0;
-assign Load_R2 = (Select == 2'd2)? Load_DST : 0;
-assign Load_R3 = (Select == 2'd3)? Load_DST : 0;
+assign LOAD_R0 = (DST_SELECTION == 2'd0)? LOAD_DST : 0;
+assign LOAD_R1 = (DST_SELECTION == 2'd1)? LOAD_DST : 0;
+assign LOAD_R2 = (DST_SELECTION == 2'd2)? LOAD_DST : 0;
+assign LOAD_R3 = (DST_SELECTION == 2'd3)? LOAD_DST : 0;
 
 endmodule
